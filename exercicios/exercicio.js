e513e81ac166b132744661d145fa87bdf2e59066
VINICIUS MANFRIN ZAMARA <26458@unifacef.edu.br>
2025-08-12 22:13:33 -0300

    Realização do exercicio 2

---
+++ exercicios/exercicio.js
@@ -0,0 +1,89 @@
+function exercicio1(){
+    let livros = []
+
+    for (let i = 0; i < 5; i++){
+        let titulo = prompt(`Informe o titulo do livro ${i+1}:`)
+        let autor = prompt(`Informe o autor do livro ${i+1}:`)
+        let ano = prompt(`Informe o ano de publicação do livro ${i+1}:`)
+        let paginas = Number(prompt(`Informe o número de páginas do livro ${i+1}:`))
+
+        let livro = {
+            titulo: titulo,
+            autor: autor,
+            ano: ano,
+            paginas: paginas
+        }
+        livros.push(livro);
+    }
+
+    let maiorDe300Paginas = []
+    let maiorQuantidadePaginas = livros[0].paginas
+    let maiorQuantidadePaginasTitulo = livros[0].titulo
+    let somaPaginas = 0
+
+    for (let i = 0; i < livros.length; i++) {
+        if (livros[i].paginas > 300){
+            maiorDe300Paginas.push(livros[i].titulo)
+        }
+
+        if (livros[i].paginas > maiorQuantidadePaginas){
+            maiorQuantidadePaginasTitulo = livros[i].titulo
+        }
+
+        somaPaginas += livros[i].paginas
+    }
+
+    let mediaPaginas = somaPaginas/livros.length
+
+    console.log("O titulo do(s) livro(s) com mais de 300 páginas é/são: ", maiorDe300Paginas)
+    console.log("O livro com maior quantidade de páginas é: ", maiorQuantidadePaginasTitulo)
+    console.log("A média de páginas de todos os livros cadastrados é: ", mediaPaginas)
+}
+
+function exercicio2(){
+    let funcionarios = []
+
+    for (let i = 0; i < 3; i++){
+        let nome = prompt(`Informe o nome do funcionário ${i+1}:`)
+        let cargo = prompt(`Informe o cargo do funcionário ${i+1}:`).toLowerCase()
+        let salario = Number(prompt(`Informe o salario do funcionário ${i+1}:`))
+        let tempoServico = Number(prompt(`Informe o tempo de serviço do funcionário ${i+1}:`))
+
+        let funcionario = {
+            nome: nome,
+            cargo: cargo,
+            salario: salario,
+            tempoServico: tempoServico
+        }
+        funcionarios.push(funcionario);
+    }
+
+    funcionarios5000e5Anos = []
+    maiorSalario = funcionarios[0].salario
+    maiorSalarioNome = funcionarios[0].nome
+    somaSalario = 0
+    funcionariosGerentes = 0
+
+    for (let i = 0; i < funcionarios.length; i++) {
+        if (funcionarios[i].salario > 5000 && funcionarios[i].tempoServico > 5){
+            funcionarios5000e5Anos.push(funcionarios[i].nome)
+        }
+        if (funcionarios[i].salario > maiorSalario){
+            maiorSalario = funcionarios[i].salario
+            maiorSalarioNome = funcionarios[i].nome
+        }
+        somaSalario += funcionarios[i].salario
+        if (funcionarios[i].cargo == "gerente"){
+            funcionariosGerentes++
+        }
+    }
+
+    mediaSalario = somaSalario/funcionarios.length
+
+    console.log("Os funcionários que ganham mais de 5000 com mais de 5 anos de serviço: ", funcionarios5000e5Anos)
+    console.log("O nome do funcionário com maior salário é: ", maiorSalarioNome)
+    console.log("A média salarial da empresa é: ", mediaSalario)
+    console.log("A empresa possui: ", funcionariosGerentes, " funcionários no cargo de gerente")
+}
+
+
